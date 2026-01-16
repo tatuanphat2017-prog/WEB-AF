71bdcac44407d1b00c3371ae6937093c9ee73b6b
Tuan Phat <tatuanphat2017@gmail.com>
2026-01-17 01:11:58 +0700

    Update .env for Railway MySQL

--- config/db.js
+++ config/db.js
@@ -2,12 +2,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+  database: process.env.DB_NAME,
+})
+
 
 db.connect((err) => {
   if (err) {
--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const authRoutes = require('./routes/authRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 require('./config/db');
 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
 dotenv.config();
 
 const app = express();
